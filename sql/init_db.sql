912bc5493bacc2cd9b93a101921a5ab2d9106dc6
Andres Galvan <afgalvan@unicesar.edu.co>
2021-05-16 23:32:12 -0500

    Fix MySQL installation

--- sql/init_db.sql
+++
@@ -1,7 +0,0 @@
-CREATE USER 'springroot'@'localhost' IDENTIFIED BY 'springroot';
-
-GRANT USAGE ON *.* TO 'springroot'@'springroot';
-
-CREATE DATABASE IF NOT EXISTS 'tasks';
-
-GRANT ALL PRIVILEGES ON 'tasks'.* TO 'springroot'@'localhost' WITH GRANT OPTION;
